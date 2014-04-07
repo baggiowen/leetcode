02c58b529741b889873a5f3aba444e74b038e8d5
baggiowen <hong.gao88@gmail.com>
2014-04-07 16:13:01 -0700

    Create two sum.java

---
+++ hashtable/two sum.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] res = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                res[0] = map.get(target - numbers[i]) + 1;
+                res[1] = i + 1;
+                break;
+            }
+            else {
+                map.put(numbers[i], i);
+            }
+        }
+        return res;
+    }
+}
+
+//solution: use hashmap to store num&index
+//map.containsKey(), not map.contains()
