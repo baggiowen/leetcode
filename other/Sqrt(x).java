7007434c09d079ccc02f7d092bf791fc28a31a51
baggiowen <hong.gao88@gmail.com>
2014-04-19 14:15:45 -0700

    Create Sqrt(x).java

---
+++ other/Sqrt(x).java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int sqrt(int x) {
+        long start = 0;
+        long end = x;
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long pow = mid * mid;
+            if (pow < x) {
+                start = mid + 1;
+            }
+            else if (pow > x) {
+                end = mid - 1;
+            }
+            else {
+                return (int)mid;
+            }
+        }
+        return (int)(start - 1);
+    }
+}
+
+//binary search
+//switch int to long to avoid overflow
+//it's ok to compare int to long
+//don't forget to case back
+//last: start - 1
