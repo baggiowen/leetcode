ff169594c64b03fe8822b3e8a08dce0fb117ac2f
baggiowen <hong.gao88@gmail.com>
2014-05-25 13:23:29 -0700

    Create Linked List Cycle II.java

---
+++ LinkedList/Linked List Cycle II.java
@@ -0,0 +1,27 @@
+http://fisherlei.blogspot.com/2013/11/leetcode-linked-list-cycle-ii-solution.html
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}
