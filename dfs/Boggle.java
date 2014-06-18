7d29a24cb6a3fb595d15f2d26c92d634de8fab07
baggiowen <hong.gao88@gmail.com>
2014-06-17 23:03:27 -0700

    Create Boggle.java

---
+++ dfs/Boggle.java
@@ -0,0 +1,112 @@
+=============
+first attempt
+=============
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Boggle {
+	public static List<String> solve(char[][] grid, HashSet<String> dict) {
+		List<String> res = new LinkedList<String>();
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				helper(res, grid, i, j, "", dict);
+			}
+		}
+		return res;
+	}
+	
+	public static void helper(List<String> res, char[][] grid, int i, int j, String word, HashSet<String> dict) {
+		char c = grid[i][j];
+		word = word + c;
+		if (word.length() >= 3) {
+			if (dict.contains(word)) {
+				res.add(new String(word));
+			}
+		}
+		
+		//top-left
+		if (i - 1 >= 0 && j - 1 >= 0 && grid[i - 1][j - 1] != '.') {
+			grid[i][j] = '.';
+			helper(res, grid, i - 1, j - 1, word, dict);
+			grid[i][j] = c;
+			word = word.substring(0, word.length());
+		}
+		
+		//top		
+		if (i - 1 >= 0 && grid[i - 1][j] != '.') {
+			grid[i][j] = '.';
+			helper(res, grid, i - 1, j, word, dict);
+			grid[i][j] = c;
+			word = word.substring(0, word.length());
+		}
+		
+		//top-right
+		if (i - 1 >= 0 && j + 1 < grid[0].length && grid[i - 1][j + 1] != '.') {
+			grid[i][j] = '.';
+			helper(res, grid, i - 1, j + 1, word, dict);
+			grid[i][j] = c;
+			word = word.substring(0, word.length());
+		}
+		
+		//right
+		if (j + 1 < grid[0].length && grid[i][j + 1] != '.') {
+			grid[i][j] = '.';
+			helper(res, grid, i, j + 1, word, dict);
+			grid[i][j] = c;
+			word = word.substring(0, word.length());
+		}
+		
+		//bottom-right
+		if (i + 1 < grid.length && j + 1 < grid[0].length && grid[i + 1][j + 1] != '.') {
+			grid[i][j] = '.';
+			helper(res, grid, i + 1, j + 1, word, dict);
+			grid[i][j] = c;
+			word = word.substring(0, word.length());
+		}
+		
+		//bottom
+		if (i + 1 < grid.length && grid[i + 1][j] != '.') {
+			grid[i][j] = '.';
+			helper(res, grid, i + 1, j, word, dict);
+			grid[i][j] = c;
+			word = word.substring(0, word.length());
+		}
+		
+		//bottom-left
+		if (i + 1 < grid.length && j - 1 >= 0 && grid[i + 1][j - 1] != '.') {
+			grid[i][j] = '.';
+			helper(res, grid, i + 1, j - 1, word, dict);
+			grid[i][j] = c;
+			word = word.substring(0, word.length());
+		}
+		
+		//left
+		if (j - 1 >= 0 && grid[i][j - 1] != '.') {
+			grid[i][j] = '.';
+			helper(res, grid, i, j - 1, word, dict);
+			grid[i][j] = c;
+			word = word.substring(0, word.length());
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		char[][] grid = new char[][]{{'a', 's', 'i'}, {'d', 'e', 'm'}, {'o', 'n', 't'}};
+		HashSet<String> dict = new HashSet<String>();
+		BufferedReader br = new BufferedReader(new FileReader("/Users/gaohong/Documents/workspace/Sorting/scowl-7.1/final/english-words.95"));
+		String line;
+		while ((line = br.readLine()) != null) {
+			dict.add(line.trim());
+		}
+		List<String> res = solve(grid, dict);
+		System.out.print(res.toString());
+		
+	}
+}
