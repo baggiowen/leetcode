c79c7fbd1c0c8b576114d6f02a550c95651c4334
baggiowen <hong.gao88@gmail.com>
2014-06-30 20:40:01 -0700

    Update Valid Number.java

--- other/Valid Number.java
+++ other/Valid Number.java
@@ -3,3 +3,48 @@ public class Solution {
         return s.matches("^\\s*[+-]?(\\d+|\\d*\\.\\d+|\\d+\\.\\d*)([eE][+-]?\\d+)?\\s*$");
     }
 }
+
+//http://leetcodenotes.wordpress.com/2013/11/23/leetcode-valid-number/
+public class Solution {
+    public boolean isNumber(String s) {
+        s = s.trim();
+        if (s.length() == 0) {
+            return false;
+        }
+        if (s.length() > 0 && s.charAt(s.length() - 1) == 'e') {
+            return false;
+        }
+        String[] tokens = s.split("e");
+        if (tokens.length > 2) {
+            return false;
+        }
+        boolean res = valid(tokens[0], false);
+        if (tokens.length > 1) {
+            res = res && valid(tokens[1], true);
+        }
+        return res;
+    }
+    
+    public boolean valid(String s, boolean hasDot) {
+        if ( s.length() > 0 && (s.charAt(0) == '+' || s.charAt(0) == '-')) {
+            s = s.substring(1);
+        }
+        if (s.length() == 0 || s.equals(".")) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '.') {
+                if (hasDot) {
+                    return false;
+                }
+                else {
+                    hasDot = true;
+                }
+            }
+            else if (s.charAt(i) < '0' || s.charAt(i) > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
