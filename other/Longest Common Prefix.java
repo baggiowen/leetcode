e07ed5882ea3293eff3709f738b2fb8e216299d2
baggiowen <hong.gao88@gmail.com>
2014-08-03 13:34:57 -0700

    Update Longest Common Prefix.java

--- other/Longest Common Prefix.java
+++ other/Longest Common Prefix.java
@@ -15,3 +15,31 @@ public class Solution {
         return res;
     }
 }
+
+
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        if (strs[0].length() == 0) {
+            return "";
+        }
+        String res = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() == 0) {
+                return "";
+            }
+            int start = 0;
+            while (start < res.length() && start < strs[i].length() && res.charAt(start) == strs[i].charAt(start)) {
+                start++;
+            }
+            if (start == 0) {
+                return "";
+            }
+            res = res.substring(0, start);
+        }
+        return res;
+    }
+}
