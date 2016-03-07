6c372b71c365336aafcb78d5c343dd9a881e9e58
baggiowen <hong.gao88@gmail.com>
2016-03-06 22:10:50 -0800

    Update Valid Parentheses.java

--- other/Valid Parentheses.java
+++ other/Valid Parentheses.java
@@ -1,39 +1,28 @@
 public class Solution {
     public boolean isValid(String s) {
-        if (s == null || s.length() == 0) {
-            return false;
-        }
         Stack<Character> stack = new Stack<Character>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '(' || c == '[' || c == '{') {
+            if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             }
             else {
                 if (stack.empty()) {
                     return false;
                 }
-                if (c == ')') {
-                    if (stack.pop() != '(') {
-                        return false;
-                    }
+                if (c == ')' && stack.pop() != '(') {
+                    return false;
                 }
-                if (c == ']') {
-                    if (stack.pop() != '[') {
-                        return false;
-                    }
+                if (c == ']' && stack.pop() != '[') {
+                    return false;
                 }
-                if (c == '}') {
-                    if (stack.pop() != '{') {
-                        return false;
-                    }
+                if (c == '}' && stack.pop() != '{') {
+                    return false;
                 }
             }
         }
-        if (!stack.empty()) {
-            return false;
-        }
-        return true;
+        
+        return stack.empty();
     }
 }
 
