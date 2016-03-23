f2bc02d01c781cdddeb7d6e4931194fb103a78df
baggiowen <hong.gao88@gmail.com>
2016-03-23 00:03:57 -0700

    Update Path Sum.java

--- Tree/Path Sum.java
+++ Tree/Path Sum.java
@@ -3,6 +3,21 @@ public class Solution {
         if (root == null) {
             return false;
         }
+        
+        if (root.left == null && root.right == null && root.val == sum) {
+            return true;
+        }
+        
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
+
+
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
         LinkedList<TreeNode> nodes = new LinkedList<TreeNode>();
         LinkedList<Integer> nums = new LinkedList<Integer>();
         nodes.add(root);
